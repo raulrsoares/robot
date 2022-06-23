4cfe6ce27d394dd13b8c5984288eb42a62542067
Raul Soares <raulrodriguessoares@gmail.com>
2022-06-22 21:01:30 -0300

    else-x

--- main/main.ino
+++ main/main.ino
@@ -55,9 +55,9 @@ void loop() {
   }
 
 
-//                                                                                                              Recebendo comando do aplicativo (F) - Inicio
+  //                                                                                                              Recebendo comando do aplicativo (F) - Inicio
   if (message == "F"); {
-    
+
     digitalWrite(mtDF, HIGH);
     digitalWrite(mtDT, LOW);
     digitalWrite(mtEF, HIGH);
@@ -65,31 +65,31 @@ void loop() {
     digitalWrite(led, HIGH);
   }
 
-//                                                                                                              Recebendo comando do aplicativo (F) - Fim
+  //                                                                                                              Recebendo comando do aplicativo (F) - Fim
 
-//                                                                                                              Recebendo comando do aplicativo (L) - Inicio
+  //                                                                                                              Recebendo comando do aplicativo (L) - Inicio
   if (message == "L") {
-    
+
     digitalWrite(mtDF, LOW);
     digitalWrite(mtDT, HIGH);
     digitalWrite(mtEF, HIGH);
     digitalWrite(mtET, LOW);
     digitalWrite(led, HIGH);
   }
-//                                                                                                              Recebendo comando do aplicativo (L) - Fim
+  //                                                                                                              Recebendo comando do aplicativo (L) - Fim
 
-//                                                                                                              Recebendo comando do aplicativo (R) - Inicio
+  //                                                                                                              Recebendo comando do aplicativo (R) - Inicio
   if (message == "R") {
-    
+
     digitalWrite(mtDF, HIGH);
     digitalWrite(mtDT, LOW);
     digitalWrite(mtEF, HIGH);
     digitalWrite(mtET, LOW);
     digitalWrite(led, HIGH);
   }
-//                                                                                                              Recebendo comando do aplicativo (R) - FIM
+  //                                                                                                              Recebendo comando do aplicativo (R) - FIM
 
-//                                                                                                              Recebendo comando do aplicativo (B) - Inicio
+  //                                                                                                              Recebendo comando do aplicativo (B) - Inicio
   if (message == "B") {
 
     digitalWrite(mtDF, LOW);
@@ -98,121 +98,78 @@ void loop() {
     digitalWrite(mtET, HIGH);
     digitalWrite(led, HIGH);
   }
-//                                                                                                              Recebendo comando do aplicativo (B) - Fim
+  //                                                                                                              Recebendo comando do aplicativo (B) - Fim
 
-//                                                                                                              Recebendo comando do aplicativo (S) - Inicio
+  //                                                                                                              Recebendo comando do aplicativo (S) - Inicio
   else if (message == "S") {
-    
+
     digitalWrite(mtDF, 0);
     digitalWrite(mtDT, LOW);
     digitalWrite(mtEF, 0);
     digitalWrite(mtET, LOW);
     digitalWrite(led, LOW);
   }
-//                                                                                                              Recebendo comando do aplicativo (S) - Fim
+  //                                                                                                              Recebendo comando do aplicativo (S) - Fim
 
-//                                                                                                              Recebendo comando do aplicativo (X) - Inicio
+  //                                                                                                              Recebendo comando do aplicativo (X) - Inicio
 
   //Aplicativo conta com comando de voz, pré configurada palavra "robonáticos", se o aplicativo entender essa palavra ele irá enviar a letra X para o ESP que irá realizar os movimentos abaixo.
-/*
   else if (message == "X") {
-    digitalWrite(led, 1);
-    digitalWrite(mtDF, 1);
-    digitalWrite(mtDT, LOW);
-    digitalWrite(mtEF, 0);
-    digitalWrite(mtET, LOW);
-    delay(300);
-    digitalWrite(led, 1);
-    digitalWrite(mtDF, 0);
-    digitalWrite(mtDT, LOW);
-    digitalWrite(mtEF, 1);
-    digitalWrite(mtET, LOW);
-    delay(300);
-    digitalWrite(mtDF, 1);
-    digitalWrite(mtDT, LOW);
-    digitalWrite(mtEF, 0);
-    digitalWrite(mtET, LOW);
-    delay(200);
-    digitalWrite(led, 1);
-    digitalWrite(mtDF, 0);
-    digitalWrite(mtDT, LOW);
-    digitalWrite(mtEF, 1);
-    digitalWrite(mtET, LOW);
-    delay(200);
-    digitalWrite(mtDF, 1);
-    digitalWrite(mtDT, LOW);
-    digitalWrite(mtEF, 0);
-    digitalWrite(mtET, LOW);
-    delay(100);
-    digitalWrite(led, 1);
-    digitalWrite(mtDF, 0);
-    digitalWrite(mtDT, LOW);
-    digitalWrite(mtEF, 1);
-    digitalWrite(mtET, LOW);
-    delay(100);
-    digitalWrite(led, 0);
-    digitalWrite(mtDF, 0);
-    digitalWrite(mtDT, LOW);
-    digitalWrite(mtEF, 0);
-    digitalWrite(mtET, LOW);
-    delay(100);
+    void MOVE(byte m1, byte m1t, byte m2, byte m2t) {
+      digitalWrite(led, 1);
+      digitalWrite(mtDF, 1);
+      digitalWrite(mtDT, m1t);
+      digitalWrite(mtEF, m2);
+      digitalWrite(mtET, m2t);
+      delay(300);
+      digitalWrite(led, 1);
+      digitalWrite(mtDF, m1);
+      digitalWrite(mtDT, m1t);
+      digitalWrite(mtEF, 1);
+      digitalWrite(mtET, m2t);
+      delay(300);
+      digitalWrite(mtDF, 1);
+      digitalWrite(mtDT, m1t);
+      digitalWrite(mtEF, m2);
+      digitalWrite(mtET, m2t);
+      delay(200);
+      digitalWrite(led, 1);
+      digitalWrite(mtDF, m1);
+      digitalWrite(mtDT, m1t);
+      digitalWrite(mtEF, 1);
+      digitalWrite(mtET, m2t);
+      delay(200);
+      digitalWrite(mtDF, 1);
+      digitalWrite(mtDT, m1t);
+      digitalWrite(mtEF, m2);
+      digitalWrite(mtET, m2t);
+      delay(100);
+      digitalWrite(led, 1);
+      digitalWrite(mtDF, m1);
+      digitalWrite(mtDT, m1t);
+      digitalWrite(mtEF, 1);
+      digitalWrite(mtET, m2t);
+      delay(100);
+      digitalWrite(led, 0);
+      digitalWrite(mtDF, m1);
+      digitalWrite(mtDT, m1t);
+      digitalWrite(mtEF, m2);
+      digitalWrite(mtET, m2t);
+      delay(100);
+      digitalWrite(mtDF, m1);
+      digitalWrite(mtDT, m1t);
+      digitalWrite(mtEF, m2);
+      digitalWrite(mtET, m2t);
+    }
   }
-
-*/
+  //                                                                                                              Recebendo comando do aplicativo (X) - Fim
 
   Serial.println(message);     //comando resposável em limpar a variavel "message"
   message = "";
 
 }
 
-void MOVE(byte m1, byte m1t, byte m2, byte m2t) {
-  digitalWrite(led, 1);
-  digitalWrite(mtDF, 1);
-  digitalWrite(mtDT, m1t);
-  digitalWrite(mtEF, m2);
-  digitalWrite(mtET, m2t);
-  delay(300);
-  digitalWrite(led, 1);
-  digitalWrite(mtDF, m1);
-  digitalWrite(mtDT, m1t);
-  digitalWrite(mtEF, 1);
-  digitalWrite(mtET, m2t);
-  delay(300);
-  digitalWrite(mtDF, 1);
-  digitalWrite(mtDT, m1t);
-  digitalWrite(mtEF, m2);
-  digitalWrite(mtET, m2t);
-  delay(200);
-  digitalWrite(led, 1);
-  digitalWrite(mtDF, m1);
-  digitalWrite(mtDT, m1t);
-  digitalWrite(mtEF, 1);
-  digitalWrite(mtET, m2t);
-  delay(200);
-  digitalWrite(mtDF, 1);
-  digitalWrite(mtDT, m1t);
-  digitalWrite(mtEF, m2);
-  digitalWrite(mtET, m2t);
-  delay(100);
-  digitalWrite(led, 1);
-  digitalWrite(mtDF, m1);
-  digitalWrite(mtDT, m1t);
-  digitalWrite(mtEF, 1);
-  digitalWrite(mtET, m2t);
-  delay(100);
-  digitalWrite(led, 0);
-  digitalWrite(mtDF, m1);
-  digitalWrite(mtDT, m1t);
-  digitalWrite(mtEF, m2);
-  digitalWrite(mtET, m2t);
-  delay(100);
-  digitalWrite(mtDF, m1);
-  digitalWrite(mtDT, m1t);
-  digitalWrite(mtEF, m2);
-  digitalWrite(mtET, m2t);
-}
-//                                                                                                              Recebendo comando do aplicativo (X) - Fim
 
 /*
-Made with @raulrsoares © 2022  Tropa do Robo, Inc
+  Made with @raulrsoares © 2022  Tropa do Robo, Inc
 */

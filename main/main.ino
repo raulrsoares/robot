e023f0367c189ab9170969cae73bf38ef038455b
Raul R. Soares <83381403+raulrsoares@users.noreply.github.com>
2022-06-23 14:36:47 -0300

    escola-colin

--- main/main.ino
+++ main/main.ino
@@ -47,6 +47,8 @@ void setup() {
 // Código de configuração aqui, para executar uma vez:
 void loop() {
 
+  MOVE(1, 0, 1, 0); // declarar MOVER, DF, DB, EF, EB
+
   if (SerialBT.available()) {
 
     char incomingChar = SerialBT.read();
@@ -99,18 +101,18 @@ void loop() {
     digitalWrite(led, HIGH);
   }
   //                                                                                                              Recebendo comando do aplicativo (B) - Fim
-/*
-  //                                                                                                              Recebendo comando do aplicativo (G) - Inicio
-  if (message == "G") {
+  /*
+    //                                                                                                              Recebendo comando do aplicativo (G) - Inicio
+    if (message == "G") {
 
-    digitalWrite(mtDF, LOW);
-    digitalWrite(mtDT, HIGH);
-    digitalWrite(mtEF, LOW);
-    digitalWrite(mtET, HIGH);
-    digitalWrite(led, HIGH);
-  }
-  //                                                                                                              Recebendo comando do aplicativo (G) - Fim
-*/
+      digitalWrite(mtDF, LOW);
+      digitalWrite(mtDT, HIGH);
+      digitalWrite(mtEF, LOW);
+      digitalWrite(mtET, HIGH);
+      digitalWrite(led, HIGH);
+    }
+    //                                                                                                              Recebendo comando do aplicativo (G) - Fim
+  */
   //                                                                                                              Recebendo comando do aplicativo (S) - Inicio
   else if (message == "S") {
 
@@ -126,53 +128,53 @@ void loop() {
 
   //Aplicativo conta com comando de voz, pré configurada palavra "robonáticos", se o aplicativo entender essa palavra ele irá enviar a letra X para o ESP que irá realizar os movimentos abaixo.
   else if (message == "X") {
-    void MOVE(byte m1, byte m1t, byte m2, byte m2t) {
-      digitalWrite(led, 1);
-      digitalWrite(mtDF, 1);
-      digitalWrite(mtDT, m1t);
-      digitalWrite(mtEF, m2);
-      digitalWrite(mtET, m2t);
-      delay(300);
-      digitalWrite(led, 1);
-      digitalWrite(mtDF, m1);
-      digitalWrite(mtDT, m1t);
-      digitalWrite(mtEF, 1);
-      digitalWrite(mtET, m2t);
-      delay(300);
-      digitalWrite(mtDF, 1);
-      digitalWrite(mtDT, m1t);
-      digitalWrite(mtEF, m2);
-      digitalWrite(mtET, m2t);
-      delay(200);
-      digitalWrite(led, 1);
-      digitalWrite(mtDF, m1);
-      digitalWrite(mtDT, m1t);
-      digitalWrite(mtEF, 1);
-      digitalWrite(mtET, m2t);
-      delay(200);
-      digitalWrite(mtDF, 1);
-      digitalWrite(mtDT, m1t);
-      digitalWrite(mtEF, m2);
-      digitalWrite(mtET, m2t);
-      delay(100);
-      digitalWrite(led, 1);
-      digitalWrite(mtDF, m1);
-      digitalWrite(mtDT, m1t);
-      digitalWrite(mtEF, 1);
-      digitalWrite(mtET, m2t);
-      delay(100);
-      digitalWrite(led, 0);
-      digitalWrite(mtDF, m1);
-      digitalWrite(mtDT, m1t);
-      digitalWrite(mtEF, m2);
-      digitalWrite(mtET, m2t);
-      delay(100);
-      digitalWrite(mtDF, m1);
-      digitalWrite(mtDT, m1t);
-      digitalWrite(mtEF, m2);
-      digitalWrite(mtET, m2t);
-    }
+
+    digitalWrite(led, 1);
+    digitalWrite(mtDF, 1);
+    digitalWrite(mtDT, m1t);
+    digitalWrite(mtEF, m2);
+    digitalWrite(mtET, m2t);
+    delay(300);
+    digitalWrite(led, 1);
+    digitalWrite(mtDF, m1);
+    digitalWrite(mtDT, m1t);
+    digitalWrite(mtEF, 1);
+    digitalWrite(mtET, m2t);
+    delay(300);
+    digitalWrite(mtDF, 1);
+    digitalWrite(mtDT, m1t);
+    digitalWrite(mtEF, m2);
+    digitalWrite(mtET, m2t);
+    delay(200);
+    digitalWrite(led, 1);
+    digitalWrite(mtDF, m1);
+    digitalWrite(mtDT, m1t);
+    digitalWrite(mtEF, 1);
+    digitalWrite(mtET, m2t);
+    delay(200);
+    digitalWrite(mtDF, 1);
+    digitalWrite(mtDT, m1t);
+    digitalWrite(mtEF, m2);
+    digitalWrite(mtET, m2t);
+    delay(100);
+    digitalWrite(led, 1);
+    digitalWrite(mtDF, m1);
+    digitalWrite(mtDT, m1t);
+    digitalWrite(mtEF, 1);
+    digitalWrite(mtET, m2t);
+    delay(100);
+    digitalWrite(led, 0);
+    digitalWrite(mtDF, m1);
+    digitalWrite(mtDT, m1t);
+    digitalWrite(mtEF, m2);
+    digitalWrite(mtET, m2t);
+    delay(100);
+    digitalWrite(mtDF, m1);
+    digitalWrite(mtDT, m1t);
+    digitalWrite(mtEF, m2);
+    digitalWrite(mtET, m2t);
   }
+
   //                                                                                                              Recebendo comando do aplicativo (X) - Fim
 
   Serial.println(message);     //comando resposável em limpar a variavel "message"
@@ -180,6 +182,14 @@ void loop() {
 
 }
 
+void MOVE(byte m1, byte m1t, byte m2, byte m2t) {
+
+digitalWrite(mtDF, m1);
+    digitalWrite(mtDT, m1t);
+    digitalWrite(mtEF, m2);
+    digitalWrite(mtET, m2t);
+    
+}
 
 /*
   Made with @raulrsoares © 2022  Tropa do Robo, Inc
